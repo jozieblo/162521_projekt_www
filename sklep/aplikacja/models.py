17191088eced5f2cc6db86be74a36026ee29c179
Ja <jarek.ozieblo@wp.pl>
2023-12-06 15:58:26 +0100

    update

--- sklep/aplikacja/models.py
+++ sklep/aplikacja/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+rodzaj_produktu = [
+        ('komputer', 'komputer'),
+        ('laptop', 'laptop'),
+        ('tablet', 'tablet'),
+        ('smartfon', 'martfon'),
+    ]
+status_zamowienia = [
+        ('Nowe', 'Nowe'),
+        ('W trakcie realizacji', 'W trakcie realizacji'),
+        ('Zrealizowane', 'Zrealizowane'),
+    ]
+
+class Uzytkownik(models.Model):
+    nazwa = models.CharField(max_length=35, unique=True)
+    email = models.CharField(max_length=50, unique=True)
+    haslo = models.CharField(max_length=20)
+    powtorz_haslo = models.CharField(max_length=20)
+
+
+class Produkt(models.Model):
+    nazwa = models.CharField(max_length=50)
+    kategoria = models.CharField(choices=rodzaj_produktu, max_length=20)
+    cena = models.CharField(max_length=20)
+    producent = models.CharField(max_length=50)
+    kolor =  models.CharField(max_length=35)
+    dostepnosc = models.IntegerField()
+    opis = models.TextField()
+
+    def __str__(self):
+        return self.nazwa
+class Zamowienie(models.Model):
+    uzytkownik = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)
+    produkty = models.ManyToManyField(Produkt) #
+    data_zamowienia = models.DateTimeField(auto_now_add=True)
+    adres_dostawy = models.CharField(max_length=70)
+    status = models.CharField(max_length=50, choices=status_zamowienia, default='Nowe')
